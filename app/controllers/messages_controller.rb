83f822798879c9275de9eae20e8eb9146a9bec56
yukirot <kamenridergaim0519@gmail.com>
2019-10-05 19:40:21 +0900

    users_management_function-fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,11 @@
 class MessagesController < ApplicationController
-  def index
- end
+  def create
+    @message = @group.messages.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました！'
+    else
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
+    end
+  end
 end
